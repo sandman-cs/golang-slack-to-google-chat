f14cf5196bc42d53b69fb834549edc0b68007788
Chris <34791945+sandman-cs@users.noreply.github.com>
2019-08-25 20:26:15 -0500

    Update struc.go

--- struc.go
+++ struc.go
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package main
 
 // Configuration File Opjects
@@ -23,3 +24,47 @@ type sourceDest struct {
 
 Message: &{{message CMF6NC8VC UM6GHFTRS <@UM6GHFTRS> has joined the channel 1565830764.000900  false [] [] <nil>  false 0 channel_join false  1565830764.000900   <nil> U0B68H1L3     [] 0 []  [] false <nil>  0 T034AG6NF []  false false {[]}} <nil> <nil>}
 */
+=======
+package main
+
+// Configuration File Opjects
+type configuration struct {
+	Channels   []sourceDest
+	AppName    string
+	AppVer     string
+	ServerName string
+	LocalEcho  bool
+	SlackToken string
+	ImagePath  string
+	ImageURL   string
+}
+
+type sourceDest struct {
+	SlackChannelName string
+	SlackChannelID   string
+	ChatURL          string
+}
+
+type imagePost struct {
+	imageLink string
+	imageURL  string
+}
+
+//Delete later if not needed.
+type chatImage struct {
+	Cards []struct {
+		Sections []struct {
+			Widgets []struct {
+				Image struct {
+					ImageURL string `json:"imageUrl"`
+					OnClick  struct {
+						OpenLink struct {
+							URL string `json:"url"`
+						} `json:"openLink"`
+					} `json:"onClick"`
+				} `json:"image"`
+			} `json:"widgets"`
+		} `json:"sections"`
+	} `json:"cards"`
+}
+>>>>>>> Stashed changes
